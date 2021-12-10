551e92b0dbae7815d4fa80a3b5ebd5dec0797c9a
mnm-54 <shahriarmun54@gmail.com>
2021-12-10 16:05:31 +0600

    created lang and manage file

---
+++ manage.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details
+ *
+ * @package    local_aquarium
+ * @author     shahriar
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @var stdClass $plugin 
+ */
+
+require_once(__DIR__ . '/../../config.php');
+global $DB;
+
+
+$PAGE->set_url(new moodle_url('/local/aquarium/manage.php'));
+$PAGE->set_context(\context_system::instance());
+$PAGE->set_title(get_string('title_manage', 'local_aquarium'));
+
+
+echo $OUTPUT->header();
+echo "<h1>hello</h1>";
+
+echo $OUTPUT->footer();
